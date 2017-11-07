27bdaf19be8eb4ac9af2982c77bf208eae2d3f03
Rodny Perez <rodnyeperez@gmail.com>
2017-11-07 18:02:19 -0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn hvccd12.wsgi --log-file -
+heroku ps:scale web=1
+
---
+++ revolution_slider/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RevolutionSliderConfig(AppConfig):
+    name = 'revolution_slider'
